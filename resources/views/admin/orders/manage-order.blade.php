659c0dac088b46c9076b480e6d2daaaebe2cfa72
Emamul Murshalin <emamul727@gmail.com>
2019-11-04 23:42:18 +0600

    Project Complete

---
+++ resources/views/admin/orders/manage-order.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emamu
+ * Date: 11/4/2019
+ * Time: 6:24 PM
+ */
---
+++ resources/views/mail/congratulation-mail.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emamu
+ * Date: 11/4/2019
+ * Time: 10:12 PM
+ */
